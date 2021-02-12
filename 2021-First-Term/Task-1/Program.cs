960f6fcc0abfc1769cc8db098b186637bdba4329
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-12 13:56:08 +0400

    yep first task

---
+++ 2021-First-Term/Task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            string x;
+
+            Console.WriteLine("Введите пять чисел: ");
+
+
+            for (int i = 0; i <= 4; i++)
+            {
+                x = Console.ReadLine();
+                s += Convert.ToInt32(x);
+            }
+
+            Console.WriteLine($"Сумма: {s}");
+        }
+    }
+}
