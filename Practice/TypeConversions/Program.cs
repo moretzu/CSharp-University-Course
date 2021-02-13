435fa089318f6ca16e27e309e42c01c5d7670e66
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-13 22:18:49 +0400

    type conversion example

---
+++ Practice/TypeConversions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Implicit Type Conversion
+            {
+                byte b = 1;
+                int i = b;
+            }
+
+            // Explicit Type Conversion
+            {
+                int i = 3000;
+                byte b = (byte)i;  // 184, data loss is happening here
+            }
+
+            {
+                float f = 1.0f;
+                int i = (int)f;
+            }
+
+            // Non-compatible types
+            {
+                string s = "1";
+                int i = Convert.ToInt32(s);
+                int j = int.Parse(s);
+            }
+        }
+    }
+}
