4fee0d47a9fc570c6858968a3ffe130186833433
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-13 22:07:20 +0400

    Overflow example

---
+++ Practice/Overflowing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Overflowing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort number = UInt16.MaxValue;
+            number++; // turns into 0
+
+            Console.WriteLine(number);
+
+            int number2 = Int32.MaxValue;
+            number2++; // turns into -2147483648
+
+            Console.WriteLine(number2);
+
+            // wrapping in checked will prevent overflows
+            // but instead will throw an exception during runtime
+            checked
+            {
+                byte number3 = 255;
+                number3++;
+            }
+        }
+    }
+}
