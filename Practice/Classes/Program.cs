cf954dfdc0d0e61f159747731425fb7fada2470a
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-14 17:09:16 +0400

    implementig the classes

---
+++ Practice/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Riku");
+            person.Introduce("reader");
+
+            var customer = new Customer(person);
+            customer.Buy("Sublime license");
+
+            var person2 = new Person("Saya");
+            person2.Introduce("everyone");
+
+            var customer2 = new Customer(person2, 1530, true, "green tea");
+            customer2.Buy("green tea");
+        }
+    }
+}
