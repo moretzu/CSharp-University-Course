892029667a3d51a54ff0c53ea4f22774676df82e
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-15 17:35:52 +0400

    yep task 2 but updated

--- 2021-Second-Term/Task-2/Program.cs
+++ 2021-Second-Term/Task-2/Program.cs
@@ -8,22 +8,20 @@ namespace Second
     {
         static void Main(string[] args)
         {
-            // Инициализируем рандомайзер и спсиок
+            // Инициализируем генератор случайных чисел
             Random randomizer = new Random();
             List<double> array = new List<double>();
 
-            // Заполним список пятью элементами
-            // Заполним случайными значениями от 1 до 10
+            // Заполним список пятью случайными числами со значениями от 1 до 10
             for (int i = 0; i < 5; i++)
-            {
                 array.Add(randomizer.Next(1, 10));
-            }
+
 
             Console.WriteLine("Элементы списка: {0}.", String.Join(", ", array));
-            
+
             // Получим ср. значение через LINQ
             double avg = array.Average();
-            Console.WriteLine($"Среднее значение: {avg}.");            
+            Console.WriteLine($"Среднее значение: {avg}.");
         }
     }
 }
