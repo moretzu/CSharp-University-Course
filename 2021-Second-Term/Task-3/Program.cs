b71c72eda977b24e9686d22168f38056b86ee1b2
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-17 11:16:06 +0400

    Some refactoring

--- 2021-Second-Term/Task-3/Program.cs
+++ 2021-Second-Term/Task-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Third
 {
@@ -6,61 +7,65 @@ namespace Third
     {
         static void PrintMatrice(int[,] arr)
         {
-            int rowLength = arr.GetLength(0);
-            int colLength = arr.GetLength(1);
+            var toWrite = new StringBuilder();
+            var rowLength = arr.GetLength(0);
+            var colLength = arr.GetLength(1);
 
-            for (int i = 0; i < rowLength; i++)
+            for (var i = 0; i < rowLength; i++)
             {
-                for (int j = 0; j < colLength; j++)
+                for (var j = 0; j < colLength; j++)
                 {
-                    Console.Write($"{arr[i, j]} ");
+                    toWrite.Append($"{arr[i, j]} ");
                 }
-                Console.WriteLine();
+
+                toWrite.Append('\n');
             }
+
+            Console.Write(toWrite.ToString());
         }
 
         static void Main(string[] args)
         {
             // Подготовим генератор случайных чисел
-            Random r = new Random();
+            var r = new Random();
 
             Console.Write("Укажите размер квадратной матрицы (одно число): ");
 
-            string inp = Console.ReadLine();
-            int n = Convert.ToInt32(inp);
+            var inp = Console.ReadLine();
+            var n = Convert.ToInt32(inp);
 
             // Сгенерируем матрицу (2-х мерный массив) указанного размера
-            int[,] matrice = new int[n, n];
+            var matrice = new int[n, n];
 
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    matrice[i, j] = r.Next(1, 10);
+            for (var i = 0; i < n; i++)
+            for (var j = 0; j < n; j++)
+                matrice[i, j] = r.Next(1, 10);
 
             Console.WriteLine("Исходная матрица: ");
             PrintMatrice(matrice);
 
             // Создадим объект транспонированной матрицы и заполним его
             // учитвая, что главная диагональ не должна изменяться
-            int[,] invertedMatrice = new int[n, n];
-            for (int i = 0; i < matrice.GetLength(0); i++)
-                for (int j = 0; j < matrice.GetLength(1); j++)
-                    if (i != j)
-                        invertedMatrice[i, j] = matrice[j, i];
-                    else
-                        invertedMatrice[i, j] = matrice[i, j];
+            var invertedMatrice = new int[n, n];
+            for (var i = 0; i < matrice.GetLength(0); i++)
+            for (var j = 0; j < matrice.GetLength(1); j++)
+                if (i != j)
+                    invertedMatrice[i, j] = matrice[j, i];
+                else
+                    invertedMatrice[i, j] = matrice[i, j];
 
             Console.WriteLine("Транспонированная матрица (главная ось сохранена)");
             PrintMatrice(invertedMatrice);
 
             // Перменная-счётчик для вычисления суммы элементов
             // главной диагонали исходной матрицы
-            int s = 0;
-            for (int i = 0; i < matrice.GetLength(0); i++)
-                for (int j = 0; j < matrice.GetLength(1); j++)
-                    if (i == j) s += matrice[i, j];
+            var s = 0;
+            for (var i = 0; i < matrice.GetLength(0); i++)
+            for (var j = 0; j < matrice.GetLength(1); j++)
+                if (i == j)
+                    s += matrice[i, j];
 
             Console.WriteLine($"Сумма элементов главной оси: {s}");
         }
     }
 }
-
